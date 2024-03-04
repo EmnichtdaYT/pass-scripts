2cef1d5e5f0aa7e9f50c21bfb7125fb9f8209af4
Zoe | Emnichtda <emnichtda@gmail.com>
2024-03-04 13:58:39 +0100

    pass chooser works now :3

--- pass-chooser.py
+++ pass-chooser.py
@@ -1,4 +1,33 @@
 import os
+import fnmatch
+import subprocess
 
-for path, directories, file_names in os.walk("~/..password-store/", followlinks=True):
-    print()
+pass_path = os.path.join(os.path.expanduser("~/.password-store"), '')
+
+
+def get_secrets():
+
+    secret_list = []
+
+    for path, directories, file_names in os.walk(pass_path, followlinks=True):
+
+        secrets = fnmatch.filter(file_names, '*.gpg')
+
+        if not secrets:
+            continue
+        
+        for secret in secrets:
+            secret_list.append(os.path.join(path[len(pass_path):], secret[:-4]))
+
+    return secret_list
+
+secrets = get_secrets()
+process = subprocess.run(["yofi", "--quiet", "--config-file", "/home/zoe/.config/yofi/password.conf", "dialog"], input='\n'.join(secrets).encode("UTF-8"), stdout=subprocess.PIPE)
+
+selected_secret = process.stdout.decode("UTF-8").strip()
+
+pass_command = ["pass", "-c", selected_secret]
+if selected_secret.endswith("/otp"):
+    pass_command.insert(1, "otp")
+
+process = subprocess.run(pass_command)
